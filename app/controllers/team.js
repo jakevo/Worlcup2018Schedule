21a87c6d2625f554e51ea430c304b82a6658be07
jakevo <javoucsd@gmail.com>
2018-05-14 23:05:38 -0700

    custom group

--- app/controllers/team.js
+++ app/controllers/team.js
@@ -1,9 +1,5 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-    check: function(params){
-        if (params == 1){
-            return true;
-        }
-    }
+
 });
